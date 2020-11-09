d826c7099bbeb0a53667b72b6ff2c91003a507b2
qStivi <stephanglaue@yahoo.de>
2020-11-09 08:52:22 +0100

    default .env file gets created now if it does not exist

--- src/main/java/qStivi/Config.java
+++ src/main/java/qStivi/Config.java
@@ -2,12 +2,43 @@ package qStivi;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Config {
 
-    private static final Dotenv DOTENV = Dotenv.load();
-
     public static String get(String key) {
-        return DOTENV.get(key);
+
+        File f = new File(System.getProperty("user.dir") + "\\.env");
+
+        Dotenv DOTENV;
+
+        if (f.exists() && !f.isDirectory()) {
+            DOTENV = Dotenv.load();
+            return DOTENV.get(key);
+        } else {
+            boolean fileCreated = false;
+            try {
+                fileCreated = f.createNewFile();
+                if (fileCreated) {
+                    FileWriter fw = new FileWriter(f);
+                    fw.write("TOKEN=Your bot token goes here");
+                    fw.write(System.lineSeparator());
+                    fw.write("PREFIX=Your command prefix goes here");
+                    fw.write(System.lineSeparator());
+                    fw.write("OWNER_ID=Your own user ID goes here. Used for admin commands");
+                    fw.write(System.lineSeparator());
+                    fw.write("CHANNEL_ID=Bot channel ID goes here");
+                    fw.write(System.lineSeparator());
+                    fw.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return "";
     }
 
 }
